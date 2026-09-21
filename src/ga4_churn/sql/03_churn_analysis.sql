5f0af3ebf77b041f8b22d6f85d366d8e7487d479
Yasin Sarıyıldız <107566832+yasinsariyildizz@users.noreply.github.com>
2026-09-21 17:50:47 +0300

    Add pre-churn classification and thresholds

--- src/ga4_churn/sql/03_churn_analysis.sql
+++ src/ga4_churn/sql/03_churn_analysis.sql
@@ -1,12 +1,17 @@
 CREATE OR REPLACE TABLE `{{PROJECT_ID}}.{{OUTPUT_DATASET_ID}}.churn_users` AS
 SELECT
   *,
+  @pre_churn_threshold AS pre_churn_threshold,
   @churn_threshold AS churn_threshold,
   CASE
     WHEN purchase_count = 0 THEN 'never_purchased'
     WHEN days_since_last_purchase > @churn_threshold THEN 'churned'
+    WHEN days_since_last_purchase > @pre_churn_threshold THEN 'pre_churn'
     ELSE 'active_purchaser'
   END AS churn_status,
+  purchase_count > 0
+    AND days_since_last_purchase > @pre_churn_threshold
+    AND days_since_last_purchase <= @churn_threshold AS pre_churn_flag,
   purchase_count > 0
     AND days_since_last_purchase > @churn_threshold AS churn_flag
 FROM `{{PROJECT_ID}}.{{OUTPUT_DATASET_ID}}.churn_base`;
