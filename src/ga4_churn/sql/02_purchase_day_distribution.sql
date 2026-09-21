30f6e66562c8c3e432048bc0487d1c656ac086f5
Yasin Sarıyıldız <107566832+yasinsariyildizz@users.noreply.github.com>
2026-09-21 17:50:37 +0300

    Switch purchase gap analysis to user_id

--- src/ga4_churn/sql/02_purchase_day_distribution.sql
+++ src/ga4_churn/sql/02_purchase_day_distribution.sql
@@ -1,25 +1,25 @@
 CREATE OR REPLACE TABLE `{{PROJECT_ID}}.{{OUTPUT_DATASET_ID}}.purchase_day_gaps` AS
 WITH purchase_days AS (
   SELECT DISTINCT
-    user_pseudo_id,
+    user_id,
     SAFE.PARSE_DATE('%Y%m%d', event_date) AS purchase_date
   FROM `{{PROJECT_ID}}.{{DATASET_ID}}.{{TABLE_ID}}`
   WHERE event_name = 'purchase'
-    AND user_pseudo_id IS NOT NULL
+    AND user_id IS NOT NULL
     AND event_date IS NOT NULL
 ),
 ordered AS (
   SELECT
-    user_pseudo_id,
+    user_id,
     purchase_date,
     LAG(purchase_date) OVER (
-      PARTITION BY user_pseudo_id
+      PARTITION BY user_id
       ORDER BY purchase_date
     ) AS previous_purchase_date
   FROM purchase_days
 )
 SELECT
-  user_pseudo_id,
+  user_id,
   previous_purchase_date,
   purchase_date,
   DATE_DIFF(purchase_date, previous_purchase_date, DAY) AS gap_days
